19513406d1c26874b3ed829e072e4cf13f50e315
Ashisid <96366098+Ashisid@users.noreply.github.com>
2021-12-31 23:03:35 +0500

    Add files via upload

---
+++ solution all output.py
@@ -0,0 +1,40 @@
+Python 3.6.4rc1 (v3.6.4rc1:3398dcb, Dec  5 2017, 20:41:32) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: C:/Users/faiza/OneDrive/Desktop/ashi/data science/patteren.py ===
+Twinkle, twinkle, little star, 
+	How I wonder what you are! 
+		Up above the world so high, 
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are!
+>>> 
+=== RESTART: C:/Users/faiza/OneDrive/Desktop/ashi/data science/patteren.py ===
+Twinkle, twinkle, little star, 
+	How I wonder what you are! 
+		Up above the world so high, 
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are!
+>>> 
+ RESTART: C:/Users/faiza/OneDrive/Desktop/ashi/data science/python version.py 
+3.6.4rc1 (v3.6.4rc1:3398dcb, Dec  5 2017, 20:41:32) [MSC v.1900 64 bit (AMD64)]
+>>> 
+ RESTART: C:/Users/faiza/OneDrive/Desktop/ashi/data science/current date and time.py 
+now = 2021-12-31 17:31:20.715785
+date and time = 31/12/2021 17:31:20
+>>> 
+ RESTART: C:/Users/faiza/OneDrive/Desktop/ashi/data science/radius of circle.py 
+Enter radius of circle : 1.1
+Area of the circle is: 3.8013271108436504
+>>> 
+ RESTART: C:/Users/faiza/OneDrive/Desktop/ashi/data science/userss first and last name.py 
+Input your First Name : ashi
+Input your Last Name : siddiqui
+Hello  siddiqui ashi
+>>> 
+== RESTART: C:/Users/faiza/OneDrive/Desktop/ashi/data science/two input.py ==
+enter first number : 4
+enter second number : 8
+sum of 4 and 8 is 12
+>>> 
---
+++ solution.py
@@ -0,0 +1,19 @@
+Python 3.6.4rc1 (v3.6.4rc1:3398dcb, Dec  5 2017, 20:41:32) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: C:/Users/faiza/OneDrive/Desktop/ashi/data science/patteren.py ===
+Twinkle, twinkle, little star, 
+	How I wonder what you are! 
+		Up above the world so high, 
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are!
+>>> 
+=== RESTART: C:/Users/faiza/OneDrive/Desktop/ashi/data science/patteren.py ===
+Twinkle, twinkle, little star, 
+	How I wonder what you are! 
+		Up above the world so high, 
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are!
+>>> 
